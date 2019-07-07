900681a1a54f4c9a5d40833f5008f228f5292ed8
Chanaka Rathnayaka <bmcrathnayaka@gmail.com>
2019-07-07 11:45:28 +0530

    added component examples codes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './ComponentExample/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hello Wossrld!</h1>
+        <Person></Person>
       </div>
     );
+    /**
+     * return (
+        <div className="App">
+          <h1>Hello Wossrld!</h1>
+        </div>
+        );
+     * above code will be compiled to the below
+     * return React.createElement('div',{className: 'App'},React.createElement('h1',null,'Hi I am Chanaka'));
+     */
+    
   }
 }
 
---
+++ src/ComponentExample/Person.js
@@ -0,0 +1,12 @@
+/**
+ *  this is a functional component
+ *  
+ */
+
+import React from 'react';
+
+const Person = () => {
+    return <p>Hello I am a Person</p>;
+}
+
+export default Person;
